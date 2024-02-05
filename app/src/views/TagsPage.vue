5140394e2b00613da581278221f727400ed8b6e9
Florian RICARD <florian.ricard@seenovate.com>
2024-02-05 08:17:09 +0000

    Ft graph component

--- app/src/views/TagsPage.vue
+++ app/src/views/TagsPage.vue
@@ -9,19 +9,22 @@
     </div>
     <div class="fr-grid-row fr-grid-row--gutters fr-py-md-3w">
       <div class="fr-col-12 fr-col-sm-6 fr-col-md-6 fr-col-xl-6">
-        <graph-box :dataObj=BoxDataA> </graph-box>
+        <graph-box :dataObj=BoxDataA :color=colors> </graph-box>
       </div>
       <div class="fr-col-12 fr-col-sm-6 fr-col-md-6 fr-col-xl-6">
-        <graph-box :dataObj=BoxDataB> </graph-box>
+        <graph-box :dataObj=BoxDataA :color=colors> </graph-box>
       </div>
     </div>
     <div class="fr-grid-row fr-grid-row--gutters fr-mb-1w">
       <div class="fr-col-12">
-        <graph-box :dataObj=BoxDataC> </graph-box>
+        <graph-box :dataObj=BoxDataA :color=colors> </graph-box>
       </div>
     </div>
     <div class="fr-grid-row">
       <div class="fr-col-12">
+            <h2 class="fr-footer__body fr-btns-group--between">Les axes pour transformer la société
+              <a class="fr-link fr-icon-arrow-right-line fr-link--icon-right" href="#">Suivre les réformes</a>
+            </h2>
         <up-footer></up-footer>
       </div>
     </div>  
@@ -46,13 +49,31 @@ export default {
   data() {
     return {
       querySuccess: true,
-
+      sujets: 'sujets',
+      colors:  ['beige-gris-galet','brown-caramel','green-bourgeon','green-menthe'],
       BoxDataA: {
           title: 'Voyages covoiturés via plateformes',
           update_date: '23/01/2024',
           description: "Description",
           source: "Talkwalker",
-          trendValue: -1
+          trendValue: -1,
+          unit: "Millions de tonnes en équivalent CO2",
+          serie_values: {
+          // x: [["2017", "2018", "2019"], ["2017", "2018", "2019"], ["2017", "2018", "2019"]],
+          x: [["2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"], 
+            ["2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"], 
+            ["2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"],
+            ["2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030"]],
+          y: [
+            [20, 18, 15, 13, 12, 16, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 14, 11, 8, 7, 6, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3]
+          ],
+          name: ['Historique', 'Année en cours', 'Projection', 'Cible']
+          },
+          horizontal: false,
+          stacked: true
       },
       BoxDataB: {
           title: 'Nombre d’infrastructures de covoiturage',
@@ -73,6 +94,12 @@ export default {
   computed: {
   },
   methods: {
+    router_to_pages (pagename) {
+        this.myrouter.push({ name: pagename })
+    },
+
+    // Range function for BoxDataA
+
 
   },
   mounted() {
