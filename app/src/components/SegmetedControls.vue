88297e0733505c4867b3cfdfd8e9cef528ae9e39
Michel KACOU <michel.kacou@seenovate.com>
2024-02-14 17:06:23 +0100

    Segmented controlers component created

---
+++ app/src/components/SegmetedControls.vue
@@ -0,0 +1,37 @@
+<template>
+    <fieldset class="fr-segmented fr-segmented--no-legend">
+    <legend class="fr-segmented__legend">
+        Légende
+    </legend>
+    <div class="fr-segmented__elements">
+        <div class="fr-segmented__element">
+            <input value="1" type="radio" id="segmented-2230-1" name="segmented-2230">
+            <label class="fr-label" for="segmented-2230-1">
+                Libellé
+            </label>
+        </div>
+        <div class="fr-segmented__element">
+            <input value="2" checked type="radio" id="segmented-2230-2" name="segmented-2230">
+            <label class="fr-label" for="segmented-2230-2">
+                Libellé
+            </label>
+        </div>
+        <div class="fr-segmented__element">
+            <input value="3" type="radio" id="segmented-2230-3" name="segmented-2230">
+            <label class="fr-label" for="segmented-2230-3">
+                Libellé
+            </label>
+        </div>
+    </div>
+</fieldset>
+</template>
+<script>
+export default {
+    data() {
+        
+    },
+}
+</script>
+<style>
+
+</style>
