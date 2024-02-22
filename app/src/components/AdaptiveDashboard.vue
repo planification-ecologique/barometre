9e2d24e296e20d392ec906ab83ccb3f2c2f20818
maria.soto <maria.soto@seenovate.com>
2024-02-22 17:07:09 +0100

    clean AdaptativeDashboard page and  add object to title and subtitle

--- app/src/components/AdaptiveDashboard.vue
+++ app/src/components/AdaptiveDashboard.vue
@@ -1,21 +1,28 @@
 <template>
-    <div v-if="hasData">
-        <div v-for="(row, index) in grid" :key="index" class="fr-grid-row fr-grid-row fr-grid-row--gutters fr-mb-1w">
-            <div v-for="(item, columnIndex) in row" :key="columnIndex" class="fr-col">
-
-               <graph-box :dataObj="item" :idAccordion="'accordion-'+ index+columnIndex" :color="colors" :displayChart="displayChart" :name=axesNames></graph-box>
+    <div class="fr-col">
+        <div class="fr-container--fluid fr-container-page">
+          <div class="fr-grid-row">
+            <div class="fr-col-12">
+              <h1 class="fr-title">{{ this.params.label_theme }}</h1>
+              <h4 class="fr-subtitle">{{ this.params.label_levier }}</h4>
+            </div>
+          </div>
+            <div v-if="hasData">
+                <div v-for="(row, index) in grid" :key="index" class="fr-grid-row fr-grid-row fr-grid-row--gutters fr-mb-1w">
+                    <div v-for="(item, columnIndex) in row" :key="columnIndex" class="fr-col">
+                    <graph-box :dataObj="item" :idAccordion="'accordion-'+ index+columnIndex" :color="colors" :displayChart="displayChart" :name=axesNames></graph-box>
+                    </div>
+                </div>
+            </div>
+            <div v-else>
+                <p>Pas de données en cours...</p>
             </div>
         </div>
-    </div>
-    <div v-else>
-        <p>Pas de données en cours...</p>
     </div>
 </template>
     
 <script>
-
 import GraphBox from '../components/GraphBox.vue'
-
 
 export default {
     name: 'AdaptiveDashboard',
@@ -28,6 +35,16 @@ export default {
             colors:  ['beige-gris-galet','brown-caramel','green-bourgeon','green-menthe'],
             displayChart: true,
             axesNames: ['Historique', 'Année en cours', 'Projection', 'Cible'],
+        }
+    },
+    props: {
+        inputData: {
+            type: Array,
+            required: true
+        },
+        params: {
+            type: Object,
+            rerquired: true
         }
     },
     computed: {
@@ -55,17 +72,26 @@ export default {
             if (lastRowNumCols === 1) {
                 grid[numRows - 1] = lastRow.slice(0, -1); // Remove the last element
             }
-
             return grid;
-
-        }
-    },
-    props: {
-        inputData: {
-            type: Array,
-            required: true
         }
     }
 }
 </script>
+
+<style scoped lang="scss">
+    .fr-container-page {
+        background-color: #F6F6F6;
+        padding-top: 1.5rem;
+        padding-left: 2.5rem;
+        padding-right: 2.5rem;
+        width: 100%;
+
+    }
+    .fr-title {
+    margin-bottom: 0.625rem;
+    }
+    .fr-subtitle {
+    font-weight: 400;
+    }
+</style>
 
