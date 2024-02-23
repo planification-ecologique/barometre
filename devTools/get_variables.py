51b41ccda337a813658532de32b64c4819cb065f
maria.soto <maria.soto@seenovate.com>
2024-02-23 16:50:08 +0100

    Test extract varaibles

--- devTools/get_variables.py
+++ devTools/get_variables.py
@@ -20,29 +20,35 @@ def main():
     parser = optparse.OptionParser(usage=usage)
 
     parser.add_option("-d", "--helm_directory", dest="directory", default=None, help="This parameter is required. Default : None")
+    parser.add_option("-e", "--environnement", dest="environnement", default=None, help="This parameter is required. Default : None")
     
     (options, args) = parser.parse_args()
 
-    if not options.directory:
+    if not options.directory or not options.environnement:
         msg = "Required parameter. More info please enter -h"
         logging.error(msg)
         print(msg)
         return failure 
             
-    file_env ='build.env'    
-    create_env_variables(file_env, options.directory)
+    file_env ='build.env'
+    create_env_variables(file_env, options.directory, options.environnement)
     return success
 
-def create_env_variables(file_env:str, helms_directory:str):
+def create_env_variables(file_env:str, helms_directory:str, env:str):
     
     file_chart = f"{helms_directory}/Chart.yaml"
     logger.info(file_chart)
     package_name, chart_name = get_package_chart_name(file_chart)
+    namespace = chart_name.replace("chart", env)
+    helm_name = chart_name.replace("-chart", '')
+    logger.info(chart_name)
     # chart_name = f"{ci_project_dir}/{chart_name}"
 
     with open(file_env, "a") as file:
         file.write(f"PACKAGE_VERSION={package_name}\n")
         file.write(f"PACKAGE_NAME={chart_name}\n")
+        file.write(f"NAMESPACE={namespace}\n")
+        file.write(f"HELM_NAME={helm_name}")
     file.close()
 
 def get_package_chart_name(file_chart:str):
@@ -60,7 +66,8 @@ def get_package_chart_name(file_chart:str):
     chart_name = chart_name[1].strip()
     package_name = chart_name + '-'+ver[1].strip()+'.tgz'
     return package_name, chart_name
-
+    
+    
 # __main__
 
 if __name__ == "__main__":
@@ -72,3 +79,4 @@ if __name__ == "__main__":
         logging.info("Answer to return: failure ({0})".format(answer))
         print("Answer to return: failure ({0})".format(answer))
     sys.exit(answer)
+    
