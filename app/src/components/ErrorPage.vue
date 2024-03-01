55da3ca0eef5f6fb1f2ea30182ee8785b94e53f5
Michel KACOU <michel.kacou@seenovate.com>
2024-03-01 12:20:09 +0100

    updt error page

--- app/src/components/ErrorPage.vue
+++ app/src/components/ErrorPage.vue
@@ -1,28 +1,41 @@
 <template>
-  <div class="fr-container--fluid ">
+  <div class="fr-container--fluid">
     <div class="fr-grid-row">
       <div class="fr-col">
         <div class="fr-container--fluid fr-container-page">
-         
-          <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--middle fr-my-7w fr-mb-md-10w">
-      <div class="fr-col-12 fr-col-offset-md-1 fr-col-md-6 fr-py-0">
-        <h1> {{ serieObj.title }}  </h1>
-        <p class="fr-text--sm fr-mb-3w">{{ serieObj.errorType }}</p>
-        <p class="fr-text--lg">La page que vous cherchez est introuvable. Excusez-nous pour la gêne occasionnée. </p>
+          <div
+            class="fr-grid-row fr-grid-row--gutters fr-grid-row--middle fr-my-7w fr-mb-md-10w"
+          >
+            <div class="fr-col-12 fr-col-offset-md-1 fr-col-md-6 fr-py-0">
+              <h1>{{ serieObj.title }}</h1>
+              <p class="fr-text--sm fr-mb-3w">{{ serieObj.errorType }}</p>
+              <p class="fr-text--lg">
+             
+                {{ serieObj.description }}
+              </p>
 
-<p class="fr-text--sm">Si vous avez tapé l’adresse web dans le navigateur, vérifiez qu’elle est correcte. La page n’est peut-être plus disponible. <br>
-Dans ce cas, pour continuer votre visite vous pouvez consulter notre page d’accueil. Sinon contactez-nous pour que l’on puisse vous rediriger vers la bonne information.</p>
+              <p class="fr-text--sm">
+                {{ serieObj.advice }}
+             
+              </p>
 
-<p><a class="fr-btn url-alias-convert" href="/">Page d'accueil</a>&nbsp; <a class="fr-btn fr-btn--secondary url-alias-convert" href="/contact" title="ouvrir Contactez-nous">Contactez-nous</a></p>
-
-      </div>
-      <div class="fr-col-12 fr-col-md-3 fr-col-offset-md-1 fr-px-6w fr-px-md-0">
-      <h1>D-Block</h1>
-      </div>
-    </div>
-
-    
-        
+              <p>
+                <a class="fr-btn url-alias-convert" href="/">Page d'accueil</a
+                >&nbsp;
+                <a
+                  class="fr-btn fr-btn--secondary url-alias-convert"
+                  href="/contact"
+                  title="ouvrir Contactez-nous"
+                  >Contactez-nous</a
+                >
+              </p>
+            </div>
+            <div
+              class="fr-col-12 fr-col-md-3 fr-col-offset-md-1 fr-px-6w fr-px-md-0"
+            >
+              <technical-error-img></technical-error-img>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -30,51 +43,36 @@ Dans ce cas, pour continuer votre visite vous pouvez consulter notre page d’ac
 </template>
 
 <script>
-import { api } from '@/services/api.js'
+import { api } from "@/services/api.js";
+import TechnicalErrorImg from "../components/components_sgv/TechnicalErrorImg.vue";
 
 export default {
-name: 'ErrorPage',
-mixins: [mixin],
-components: {
-},
-data () {
-    return {
-  
-    }
-},
-props:{
-    serieObj:{
-        type: Object,
-        required: true
+  name: "ErrorPage",
+  components: {
+    TechnicalErrorImg,
+  },
+  data() {
+    return {};
+  },
+  props: {
+    serieObj: {
+      type: Object,
+      required: true,
     },
-    
-},
-methods: {
-    setWidgetId () {
-    },
-    get_colors () {
-   
-    }
-},
-mounted () {
-  
-},
-watch: {
- 
-}, 
-}
+  },
+};
 </script>
 
 <style scoped lang="scss">
-  .fr-container-page {
-    background-color: white;
-    padding-top: 1.5rem;
-    padding-left: 2.5rem;
-    padding-right: 2.5rem;
-    width: 100%;
-  }
+.fr-container-page {
+  background-color: white;
+  padding-top: 1.5rem;
+  padding-left: 2.5rem;
+  padding-right: 2.5rem;
+  width: 100%;
+}
 
-  .fr-subtitle {
-    font-weight: 400;
-    }
+.fr-subtitle {
+  font-weight: 400;
+}
 </style>
--- app/src/views/Error400Page.vue
+++ app/src/views/Error400Page.vue
@@ -18,7 +18,7 @@
               <p class="fr-text--sm">
                 Si vous avez tapé l’adresse web dans le navigateur, vérifiez
                 qu’elle est correcte. La page n’est peut-être plus
-                disponible.<br />
+                disponible.
                 Dans ce cas, pour continuer votre visite vous pouvez consulter
                 notre page d’accueil. Sinon contactez-nous pour que l’on puisse
                 vous rediriger vers la bonne information.
