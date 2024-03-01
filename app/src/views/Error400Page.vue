f9bc0a5fb41225c10d46204169109da347cb558f
Michel KACOU <michel.kacou@seenovate.com>
2024-03-01 15:12:07 +0100

    updt error pages

--- app/src/views/Error400Page.vue
+++ app/src/views/Error400Page.vue
@@ -3,44 +3,8 @@
     <div class="fr-grid-row">
       <div class="fr-col">
         <div class="fr-container--fluid fr-container-page">
+              <error-page :serieObj=cardError></error-page>
           
-          <div
-            class="fr-grid-row fr-grid-row--gutters fr-grid-row--middle fr-my-7w fr-mb-md-10w"
-          >
-            <div class="fr-col-12 fr-col-offset-md-1 fr-col-md-6 fr-py-0">
-              <h1>Requête incorrecte</h1>
-              <p class="fr-text--sm fr-mb-3w">Erreur 400</p>
-              <p class="fr-text--lg">
-                Nous sommes désolé, mais vous n'avez pas accès à cette page ou
-                cette ressource
-              </p>
-
-              <p class="fr-text--sm">
-                Si vous avez tapé l’adresse web dans le navigateur, vérifiez
-                qu’elle est correcte. La page n’est peut-être plus
-                disponible.
-                Dans ce cas, pour continuer votre visite vous pouvez consulter
-                notre page d’accueil. Sinon contactez-nous pour que l’on puisse
-                vous rediriger vers la bonne information.
-              </p>
-
-              <p>
-                <a class="fr-btn url-alias-convert" href="/">Page d'accueil</a
-                >&nbsp;
-                <a
-                  class="fr-btn fr-btn--secondary url-alias-convert"
-                  href="/contact"
-                  title="ouvrir Contactez-nous"
-                  >Contactez-nous</a
-                >
-              </p>
-            </div>
-            <div
-              class="fr-col-12 fr-col-md-3 fr-col-offset-md-1 fr-px-6w fr-px-md-0"
-            >
-              <technical-error-img></technical-error-img>
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -48,15 +12,32 @@
 </template>
 
 <script>
+import { api } from "@/services/api.js";
+import ErrorPage from '../components/ErrorPage.vue'
 import TechnicalErrorImg from "../components/components_sgv/TechnicalErrorImg.vue";
 
 export default {
   name: "Error404Page",
   components: {
     TechnicalErrorImg,
+    ErrorPage
   },
   data() {
-    return {};
+    return {
+      querySuccess : true,
+      cardError:{
+        title:'Requête incorrecte',
+        errorType:"Erreur 400",
+        description:`   Nous sommes désolé, mais vous n'avez pas accès à cette page ou
+                cette ressource.`,
+        advice:`   Si vous avez tapé l’adresse web dans le navigateur, vérifiez
+                qu’elle est correcte. La page n’est peut-être plus disponible.
+                
+                Dans ce cas, pour continuer votre visite vous pouvez consulter
+                notre page d’accueil. Sinon contactez-nous pour que l’on puisse
+                vous rediriger vers la bonne information.`,
+      }
+    };
   },
   methods: {},
   mounted() {},
--- app/src/views/Error403Page.vue
+++ app/src/views/Error403Page.vue
@@ -3,47 +3,8 @@
     <div class="fr-grid-row">
       <div class="fr-col">
         <div class="fr-container--fluid fr-container-page">
-          <!-- <div> <h1>
-            Styles P 
-          </h1>
-          </div> -->
-          <div
-            class="fr-grid-row fr-grid-row--gutters fr-grid-row--middle fr-my-7w fr-mb-md-10w"
-          >
-            <div class="fr-col-12 fr-col-offset-md-1 fr-col-md-6 fr-py-0">
-              <h1>Accès Interdit</h1>
-              <p class="fr-text--sm fr-mb-3w">Erreur 403</p>
-              <p class="fr-text--lg">
-                Nous sommes désolé, mais vous n'avez pas accès à cette page ou
-                cette ressource
-              </p>
-
-              <p class="fr-text--sm">
-                Si vous avez tapé l’adresse web dans le navigateur, vérifiez
-                qu’elle est correcte. La page n’est peut-être plus
-                disponible.<br />
-                Dans ce cas, pour continuer votre visite vous pouvez consulter
-                notre page d’accueil. Sinon contactez-nous pour que l’on puisse
-                vous rediriger vers la bonne information.
-              </p>
-
-              <p>
-                <a class="fr-btn url-alias-convert" href="/">Page d'accueil</a
-                >&nbsp;
-                <a
-                  class="fr-btn fr-btn--secondary url-alias-convert"
-                  href="/contact"
-                  title="ouvrir Contactez-nous"
-                  >Contactez-nous</a
-                >
-              </p>
-            </div>
-            <div
-              class="fr-col-12 fr-col-md-3 fr-col-offset-md-1 fr-px-6w fr-px-md-0"
-            >
-              <technical-error-img></technical-error-img>
-            </div>
-          </div>
+              <error-page :serieObj=cardError></error-page>
+          
         </div>
       </div>
     </div>
@@ -52,15 +13,31 @@
 
 <script>
 import { api } from "@/services/api.js";
+import ErrorPage from '../components/ErrorPage.vue'
 import TechnicalErrorImg from "../components/components_sgv/TechnicalErrorImg.vue";
 
 export default {
   name: "Error404Page",
   components: {
     TechnicalErrorImg,
+    ErrorPage
   },
   data() {
-    return {};
+    return {
+      querySuccess : true,
+      cardError:{
+        title:'Accès Interdit',
+        errorType:"Erreur 403",
+        description:`  Nous sommes désolé, mais vous n'avez pas accès à cette page ou
+                cette ressource.`,
+        advice:`   Si vous avez tapé l’adresse web dans le navigateur, vérifiez
+                qu’elle est correcte. La page n’est peut-être plus disponible.
+                
+                Dans ce cas, pour continuer votre visite vous pouvez consulter
+                notre page d’accueil. Sinon contactez-nous pour que l’on puisse
+                vous rediriger vers la bonne information.`,
+      }
+    };
   },
   methods: {},
   mounted() {},
