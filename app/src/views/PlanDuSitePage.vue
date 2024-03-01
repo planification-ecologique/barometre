5543a40fcb277fd90e80776e491067c368a98b7f
Michel KACOU <michel.kacou@seenovate.com>
2024-03-01 14:38:52 +0100

    plan du site updt

---
+++ app/src/views/PlanDuSitePage.vue
@@ -0,0 +1,198 @@
+<template>
+  <main role="main" id="content" class="fr-container fr-py-16v">
+    <div class="fr-grid-row">
+      <div class="fr-col-12 fr-col-md-8">
+        <h1>Plan du site</h1>
+        <section class="fr-py-6v">
+          <ul>
+            <li><a href="/" class="router-link-active">Accueil</a></li>
+            <li>
+              <a href="/utilisation-et-organisation" class="" target="_self"
+                >Tableau de bord</a
+              >
+              <!-- <ul>
+                <li>
+                  <a
+                    href="/utilisation-et-organisation/perimetre-d-application"
+                    class=""
+                    target="_self"
+                  >
+                    Périmètre d'application</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/utilisation-et-organisation/procedure-des-agrements"
+                    class=""
+                    target="_self"
+                    >Procédure des agréments</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/utilisation-et-organisation/designers"
+                    class=""
+                    target="_self"
+                    >Designers</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/utilisation-et-organisation/developpeurs"
+                    class=""
+                    target="_self"
+                    >Développeurs</a
+                  >
+                </li>
+              </ul>
+            </li> -->
+            <li>
+              <a href="/elements-d-interface" class="" target="_self"
+                >Tags</a
+              >
+              <!-- <ul>
+                <li>
+                  <a
+                    href="/elements-d-interface/fondamentaux-de-l-identite-de-l-etat"
+                    class=""
+                    target="_self"
+                    >Fondamentaux de l'identité de l'État</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/elements-d-interface/fondamentaux-techniques"
+                    class=""
+                    target="_self"
+                    >Fondamentaux techniques</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/elements-d-interface/composants"
+                    class=""
+                    target="_self"
+                    >Composants</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/elements-d-interface/blocs-fonctionnels"
+                    class=""
+                    target="_self"
+                    >Blocs fonctionnels</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/elements-d-interface/modeles"
+                    class=""
+                    target="_self"
+                    >Modèles</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/elements-d-interface/composants-beta"
+                    class=""
+                    target="_self"
+                    >Composants bêta</a
+                  >
+                </li>
+              </ul> -->
+            </li>
+            <!-- <li>
+              <a href="/communaute" class="" target="_self">Communauté</a>
+              <ul>
+                <li>
+                  <a
+                    href="/communaute/rejoindre-la-communaute"
+                    class=""
+                    target="_self"
+                    >Rejoindre la communauté</a
+                  >
+                </li>
+                <li>
+                  <a href="/communaute/exemple-de-sites" class="" target="_self"
+                    >Exemple de sites</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/communaute/portages-en-cours"
+                    class=""
+                    target="_self"
+                    >Portages du DSFR</a
+                  >
+                </li>
+                <li>
+                  <a href="/communaute/evenements" class="" target="_self"
+                    >Événements</a
+                  >
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="/centre-de-support" class="" target="_self"
+                >Centre de support</a
+              >
+            </li>
+            <li>
+              <a href="/a-propos" class="" target="_self">À propos</a>
+              <ul>
+                <li>
+                  <a href="/a-propos/versions" class="" target="_self"
+                    >Versions</a
+                  >
+                </li>
+                <li>
+                  <a href="/a-propos/articles" class="" target="_self"
+                    >Articles</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/a-propos/equipe-du-systeme-de-design"
+                    class=""
+                    target="_self"
+                    >L'équipe DSFR</a
+                  >
+                </li>
+                <li>
+                  <a href="/a-propos/roadmap" class="" target="_self"
+                    >Feuille de route</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/a-propos/conditions-generales-d-utilisation"
+                    class=""
+                    target="_self"
+                    >Conditions générales d'utilisation</a
+                  >
+                </li>
+                <li>
+                  <a
+                    href="/a-propos/configuration-minimale-requise-pour-utiliser-le-dsfr"
+                    class=""
+                    target="_self"
+                    >Configuration minimale requise</a
+                  >
+                </li>
+              </ul>
+            </li> -->
+            <li>
+              <a href="/accessibilite" class="" target="_self">Accessibilité</a>
+              <!---->
+            </li>
+            <li><a href="/mentions-legales" class="">Mentions légales</a></li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  </main>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
