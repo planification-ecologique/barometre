1bd3a5eeb1cc05258a7bd52ec26989743f355139
Chouaib BRAKNI <chouaib.brakni@seenovate.com>
2024-03-07 17:26:53 +0100

    Grid of graphs

--- app/src/components/AdaptiveDashboard.vue
+++ app/src/components/AdaptiveDashboard.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <div class="fr-grid-row" v-if="dashboardPage">
+            <!-- <div class="fr-col-1">
+                <img :src="'/img/' + this.params.label_theme  + '.png'" alt="Image" style="pointer-events: auto; opacity: 1; width: 92.7859px; height: 92.7859px; vertical-align: top; transform: scale(1, 1)">
+            </div> -->
             <div class="fr-col-12">
                 <h1 class="fr-title">{{ this.params.label_theme }}</h1>
                 <h4 class="fr-subtitle">{{ this.params.label_levier }}</h4>
@@ -10,10 +13,10 @@
             <div v-for="(row, index) in gridFormat" :key="index" class="fr-grid-row fr-grid-row fr-grid-row--gutters fr-mb-1w">
                 <div v-for="(item, columnIndex) in row" :key="columnIndex" class="fr-col">
                     <div v-if="!item">
-                        Pas d'items
                     </div>
                     <div v-else>
-                        <graph-box :dataObj="item" :idAccordion="'accordion-'+ index+columnIndex"></graph-box>
+                        <graph-box :dataObj="item" :idAccordion="'accordion-'+ index+columnIndex"></graph-box> 
+                        
                     </div>
                 </div>
             </div>
@@ -52,26 +55,24 @@ export default {
     },
     methods: {
         grid(ls_indicateurs) {
-            const numRows = Math.ceil(ls_indicateurs.length / 2);
+            const numRows = Math.ceil(ls_indicateurs.length / 2) || 1; 
             const numCols = 2;
 
             const grid = Array.from({ length: numRows }, () => Array(numCols).fill(null));
 
-            // create the grid with the data using a nested loop
             ls_indicateurs.forEach((item, index) => {
-                const row = Math.floor(index / numCols); // integer division
-                const col = index % numCols; // remainder of division
+                const row = Math.floor(index / numCols); 
+                const col = index % numCols; 
                 grid[row][col] = item
             });
-            const lastRow = grid[numRows - 1];
-            const lastRowNumCols = lastRow.filter(item => item !== null).length;
 
-            // Adjust the number of columns for the last row
-            if (lastRowNumCols === 1) {
-                grid[numRows - 1] = lastRow.slice(0, -1); // Remove the last element
+            if (ls_indicateurs.length % 2 === 1) {
+                const lastRowIndex = numRows - 1;
+                grid[lastRowIndex][numCols - 1] = null;
             }
+
             return grid;
-        }
+        },
     },
     watch: {
         inputData: {
