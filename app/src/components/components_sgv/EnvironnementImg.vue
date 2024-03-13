7a5c658997fc79a18ea804bb1ac1ee8434f13dcd
Michel KACOU <michel.kacou@seenovate.com>
2024-03-13 15:53:32 +0100

    picto links removed

--- app/src/components/components_sgv/EnvironnementImg.vue
+++ app/src/components/components_sgv/EnvironnementImg.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="fr-card__img">
-    <a href="/accueil" target="_self">
       <svg
         :width="width"
         :height="height"
@@ -94,7 +93,6 @@
           />
         </g>
       </svg>
-    </a>
     <!-- L’alternative de l’image (attribut alt) doit toujours être présente, sa valeur peut-être vide (image n’apportant pas de sens supplémentaire au contexte) ou non (porteuse de texte ou apportant du sens) selon votre contexte -->
   </div>
 </template>
