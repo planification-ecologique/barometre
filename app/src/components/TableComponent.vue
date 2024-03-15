a7b655983fdc6bcdade00da89b51aec37220fd8c
Michel KACOU <michel.kacou@seenovate.com>
2024-03-15 16:24:48 +0100

    fix conflict

--- app/src/components/TableComponent.vue
+++ app/src/components/TableComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="fr-table fr-table--layout-fixed fr-tab-data fr-ml-1w fr-mr-1w fr-mt-1w fr-mb-2w">
         <table class="">          
-            <caption class="sr-only">{{ captionTitle }}</caption>
+            <caption class="sr-only">{{ captionTitle }}</caption> <!-- Ajout de la classe sr-only -->
             <thead>
                 <tr>
                     <th scope="col">Année</th>
@@ -21,13 +21,12 @@
 <script>
 export default {
     name: 'TableComponent',
-    components: {
-    },
     props: {
-       captionTitle: {
-      type: String,
-      required: true
-    },
+        captionTitle: {
+            type: String,
+            required: true,
+            default: ''
+        },
         annee: {
             type: [],
             required: true,
@@ -55,6 +54,7 @@ export default {
     box-shadow: 0 16px 16px rgba(18, 14, 153, 0.6); /* Augmentez l'intensité de l'ombre */
 }
 
+/* Ajout de la classe sr-only */
 .sr-only {
   position: absolute;
   width: 1px;
