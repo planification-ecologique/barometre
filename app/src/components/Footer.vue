0660be1a0330177162f4cc03cd9055008a589f20
maria.soto <maria.soto@seenovate.com>
2024-03-27 15:20:40 +0100

    Router link with footer

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
   <footer class="fr-footer" role="contentinfo" id="footer">
-    <div class="footer-container">
+    <div class="fr-container--fluid fr-ml-2w">
       <div class="fr-footer__body">
         <div class="fr-footer__brand fr-enlarge-link">
           <a
             href="https://www.gouvernement.fr/"
             target="_blank"
-            id="logo-footer-href"
+            id="fr-footer-logo-footer-href"
             title="Retour à l’accueil du site - Nom de l’entité (ministère, secrétariat d‘état, gouvernement)"
           >
             <p class="fr-logo">GOUVERNEMENT</p>
@@ -18,94 +18,109 @@
             Ecologique (SGPE).
           </p>
           <ul class="fr-footer__content-list">
-            <li class="fr-footer__content-item">
+            <li class="fr-footer__content-item" v-for="option in menuLinks">
               <a
                 class="fr-footer__content-link"
-                id="link-legifrance"
+                :id="'fr-footer-link-'+ option.value"
                 target="_blank"
                 rel="noopener external"
-                title="legifrance.gouv.fr"
-                href="https://legifrance.gouv.fr"
-                >legifrance.gouv.fr</a
-              >
-            </li>
-            <li class="fr-footer__content-item">
-              <a
-                class="fr-footer__content-link"
-                id="link-gouvernemment"
-                target="_blank"
-                rel="noopener external"
-                title="gouvernement.fr"
-                href="https://gouvernement.fr"
-                >gouvernement.fr</a
-              >
-            </li>
-            <li class="fr-footer__content-item">
-              <a
-                class="fr-footer__content-link"
-                id="link-service-public"
-                target="_blank"
-                rel="noopener external"
-                title="service-public.fr"
-                href="https://service-public.fr"
-                >service-public.fr</a
-              >
-            </li>
-            <li class="fr-footer__content-item">
-              <a
-                class="fr-footer__content-link"
-                id="link-data-gouv"
-                target="_blank"
-                rel="noopener external"
-                title="data.gouv.fr"
-                href="https://data.gouv.fr"
-                >data.gouv.fr</a
+                :title=option.label 
+                :href=option.link
+                >{{ option.label }}</a
               >
             </li>
           </ul>
         </div>
       </div>
-      <navigation-footer></navigation-footer>
-      <div class="fr-footer__bottom-copy">
-        <p>
-          Sauf mention explicite de propriété intellectuelle détenue par des
-          tiers, les contenus de ce site sont proposés sous
-          <a
-            href="https://github.com/etalab/licence-ouverte/blob/master/LO.md"
-            id="link-license"
-            target="_blank"
-            rel="noopener external"
-            title="licence etalab-2.0"
-            >licence etalab-2.0</a
-          >
-        </p>
+      <div class="fr-footer__bottom">
+        <ul class="fr-footer__bottom-list">
+          <li class="fr-footer__bottom-item" v-for="option in menuOptions">
+            <router-link
+              class="fr-footer__bottom-link"
+              target="_self"
+              :id="'fr-footer-link-'+ option.router_name"
+              :to="{ name: option.router_name }"
+              :title=option.label
+              > {{ option.label }}
+            </router-link>
+          </li>
+
+          <li class="fr-footer__bottom-item">
+            <a
+              id="fr-footer-link-cookies"
+              class="fr-footer__bottom-link"
+              href="#"
+              onclick="tarteaucitron.userInterface.openPanel();"
+              title="Gestion des cookies"
+              >Gestion des cookies</a
+            >
+          </li>
+        </ul>
+        <div class="fr-footer__bottom-copy">
+          <p>
+            Sauf mention explicite de propriété intellectuelle détenue par des
+            tiers, les contenus de ce site sont proposés sous
+            <a
+              href="https://github.com/etalab/licence-ouverte/blob/master/LO.md"
+              id="link-license"
+              target="_blank"
+              rel="noopener external"
+              title="licence etalab-2.0"
+              >licence etalab-2.0</a
+            >
+          </p>
+        </div>
       </div>
     </div>
   </footer>
 </template>
 
 <script>
-import NavigationFooter from "./NavigationFooter.vue";
-
 export default {
   name: "FooterDsfr",
-  components: {
-    NavigationFooter,
-  },
   data() {
-    return {};
-  },
-  computed: {},
-  methods: {},
-  mounted() {},
-};
-</script>
-<style>
-.footer-container {
-  width: 100%;
-  margin-left: auto;
-  margin-right: auto;
-  padding-left: 1rem;
-  padding-right: 1rem;
+    return {
+      menuOptions: [
+        {
+          label: "Plan du site",
+          router_name: "plan-du-site",
+        },
+        {
+          label: "Accessibilité : non/partiellement/totalement conforme",
+          router_name: "accessibilite"
+        },
+        {
+          label: "Mentions légales",
+          router_name: "mentions-legales"
+        },
+        {
+          label: "Données personnelles",
+          router_name: "donnees-personnelles"
+        }       
+      ],
+      menuLinks: [
+        {
+          link: "https://legifrance.gouv.fr",
+          label: "legifrance.gouv.fr",
+          value: "legifrance"
+        },
+        {
+          link: "https://gouvernement.fr",
+          label: "gouvernement.fr",
+          value: "gouvernemment"
+        },
+        {
+          link: "https://service-public.fr",
+          label: "service-public.fr",
+          value: "service-public"
+        },
+        {
+          link: "https://data.gouv.fr",
+          label: "data.gouv.fr",
+          value: "data-gouv"
+        }
+      ]
+    }
+  }
 }
-</style>
+</script>
