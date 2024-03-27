7daccb945496b5671be48c14cfed85cadc0eaa21
maria.soto <maria.soto@seenovate.com>
2024-03-27 14:15:51 +0100

    Add  skip-link

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="fr-container--fluid">
       <div id="app">
+        <SkipLinksDsfr></SkipLinksDsfr>
         <header-dsfr />
         <main id="content">
           <router-view />
@@ -16,18 +17,20 @@
 
 import HeaderDsfr from "./components/Header.vue";
 import FooterDsfr from "./components/Footer.vue";
+import SkipLinksDsfr from "./components/components_dsfr/SkipLinks.vue";
 
 export default {
   name: "App",
   components: {
     HeaderDsfr,
     FooterDsfr,
+    SkipLinksDsfr
   },
 
   watch: {
     $route(to, from) {
       document.title = to.meta.title || "Baromètre de la planification écologique";
-    },
-  },
+    }
+  }
 };
 </script>
---
+++ app/src/components/components_dsfr/SkipLinks.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="fr-skiplinks">
+    <nav class="fr-container" role="navigation" aria-label="Accès rapide">
+      <ul class="fr-skiplinks__list">
+        <li v-for="option in menuOptions">
+          <a :id="'fr-skip-link-'+ option.value" class="fr-link" :href=option.link>
+            {{ option.label }}
+          </a >
+        </li>        
+      </ul>
+      </nav>
+  </div>
+</template>
+  
+<script>
+  export default {
+    name: "SkipLinksDsfr",
+    data(){
+      return {
+        menuOptions: [
+          { value:"content", label: 'Contenu', link: "#content" },
+          { value:"header-navigation", label: 'Menu', link: "#header-navigation" },
+          { value:"footer", label: 'Pied de page', link: "#footer" }
+        ]
+      }
+    }
+  }
+</script>
+  
