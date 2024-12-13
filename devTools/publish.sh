9edb52ce4809728dfd980f8d6a1db8f6cc1cbe2c
nicolas.arias <nicolas.arias@developpement-durable.gouv.fr>
2024-12-13 13:44:31 +0100

    change registry reference

--- devTools/publish.sh
+++ devTools/publish.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
 #Logging to repository
-GIT_HOST=gitlab.seenovate.com:5050
+GIT_HOST=registry.gitlab-forge.din.developpement-durable.gouv.fr
 # echo $GIT_HOST
 docker login $GIT_HOST
 
 #Build image
-CONTAINER_REGISTRY=sgpe/sgpe/sgpe-app 
+CONTAINER_REGISTRY=cgdd/sri/ecolab/indicateurs-territoriaux/tableau-de-bord-de-la-planification-ecologique-sgpe/sgpe-app 
 BRANCH_GIT=$(git symbolic-ref --short HEAD)
 TAG_NUMBER="0.0.1"
 IMAGE_TAG_NAME=barometre-sgpe
@@ -16,14 +16,16 @@ echo $BRANCH_GIT
 if [ "$BRANCH_GIT" != "main" ]
 then
     IMAGE_TAG_NAME=$IMAGE_TAG_NAME-$BRANCH_GIT
+else
+    IMAGE_TAG_NAME=$IMAGE_TAG_NAME-prod
 fi
 
 IMAGE_NAME=$GIT_HOST"/"$CONTAINER_REGISTRY"/"$IMAGE_TAG_NAME:$TAG_NUMBER
 echo $IMAGE_NAME
-docker rmi $IMAGE_NAME
 
 docker build --no-cache --tag $IMAGE_NAME .
 
 # push to git repository
 docker push $IMAGE_NAME
 
+docker rmi $IMAGE_NAME
