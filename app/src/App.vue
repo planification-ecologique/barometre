76bcba7d4de18f70fcc2df48abe2235ed1f52ede
ericboucher <eric.p.boucher@gmail.com>
2025-06-27 11:19:23 +0200

    Fix css

--- app/src/App.vue
+++ app/src/App.vue
@@ -64,11 +64,4 @@ body.has-staging-banner {
   padding-top: 80px;
 }
 
-#app {
-  font-family: 'Marianne', Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 </style>
