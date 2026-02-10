6c4e8af30e852db343c1f935954b665fb22fc4e5
ericboucher <eric.p.boucher@gmail.com>
2026-02-10 18:22:54 +0100

    Update AboutPage.vue

--- app/src/views/AboutPage.vue
+++ app/src/views/AboutPage.vue
@@ -3,20 +3,40 @@
     <h1 class="fr-h1">À propos du baromètre de la planification écologique</h1>
 
     <p>
-      Entreprises, pouvoirs publics ou citoyens, la transition écologique nous concerne tous. Pour réussir, elle demande de la transparence sur le chemin à parcourir, et une vision claire des objectifs à atteindre en faveur du climat, de la biodiversité et de la préservation des ressources. C’est l’objet du baromètre de la planification écologique.
+      Entreprises, pouvoirs publics ou citoyens, la transition écologique nous concerne tous. Pour réussir, elle demande de la transparence sur le chemin à parcourir, et une vision claire des objectifs à atteindre en faveur du climat, de la biodiversité ou encore de la préservation des ressources. <strong>C'est l'objet du baromètre de la planification écologique.</strong>
     </p>
 
     <p>
-      Les 200+ indicateurs regroupés dans ce tableau de bord proposent une vision quantifiée et partagée des leviers clés de la planification, tels qu’ils sont estimés à ce jour, dans les transports, le bâtiment, l’énergie, l’industrie, l’agriculture, l’alimentation, l’eau, les écosystèmes naturels et l’économie circulaire, en les mettant en regard de leurs éventuelles cibles à l’horizon 2030.
+      Fort de <strong>plus de 150 indicateurs</strong>, ce baromètre propose une <strong>vision quantifiée et partagée</strong> des actions qu'il conviendrait de mener pour accélérer la transition dans les transports, le bâtiment, l'énergie, l'industrie, l'agriculture, l'alimentation, l'eau, les écosystèmes naturels et l'économie circulaire, en les mettant en regard de leurs éventuelles cibles à horizon 2030. Sans oublier les <strong>données régionales</strong> qui sont désormais visualisables lorsque celles-ci sont disponibles, afin de matérialiser les résultats de la territorialisation des politiques publiques environnementales.
     </p>
 
     <p>
-      La lecture de ce baromètre permet d’appréhender l’ampleur et la complexité des changements à opérer, tout en révélant, levier par levier, le caractère atteignable de ces objectifs si chacun, selon ses responsabilités, prend la juste mesure du travail à entreprendre. Les trajectoires présentées reposent sur des objectifs officiels ou des hypothèses techniques, et peuvent donc évoluer. Elles visent à donner une vision cohérente des stratégies nationales existantes ou en cours d’élaboration. Ces indicateurs peuvent ainsi éclairer les acteurs concernés et soutenir le débat collectif.
+      Consulter ce baromètre, c'est appréhender l'ampleur et la complexité des changements à opérer, tout en révélant le caractère réalisable de ces objectifs. Ainsi, les projections présentées sont de deux natures :
     </p>
 
+    <ul>
+      <li><strong>la trajectoire tendancielle</strong> qui correspond à une extrapolation des données enregistrées depuis 2023, date initiale des travaux de planification écologique et également année de base des budgets carbone du projet de Stratégie Nationale Bas-Carbone ;</li>
+      <li><strong>et la trajectoire souhaitable</strong> pour atteindre ces cibles, que celles-ci soient annualisées ou non. Les cibles indiquées, lorsqu'elles existent, proviennent de sources diverses : fichiers techniques non publics, documents de référence, voire jalons réglementaires.</li>
+    </ul>
+
     <p>
-      Réalisé avec le commissariat général au développement durable (CGDD), le baromètre de la planification écologique est le fruit d’un travail interministériel essentiel qui va se poursuivre en intégrant à la fois de nouvelles données (adaptation au changement climatique, objectifs post-2030, vision territoriale…) et de nouveaux enjeux (disponibilité de la donnée, coordination avec les autres bases de données existantes, tableaux spécifiques pour certains territoires, notamment ultramarins, etc.). Les données utilisées viennent de bases publiques, de rapports officiels et d'études scientifiques, régulièrement mis à jour. Les sources sont précisées pour chaque indicateur, avec accès possible aux données brutes.
+      Fruit d'un <strong>travail interministériel essentiel</strong> qui va se poursuivre par l'intégration de <strong>nouvelles données</strong> (implémentation d'indicateurs des futures stratégies nationales, objectifs post-2030...) <strong>et de nouveaux enjeux</strong> (coordination avec de nouvelles bases, tableaux spécifiques pour certains territoires, notamment ultramarins, etc.), ce baromètre <strong>bénéficiera de mises à jour régulières</strong>. Les sources sont précisées pour chaque indicateur, avec accès possible aux données brutes.
     </p>
+
+    <div class="fr-callout fr-mt-4w">
+      <h2 class="fr-callout__title">Pour aller + loin</h2>
+      <p>
+        Le baromètre de la planification écologique a vu sa <strong>structure approfondie par rapport à sa version précédente</strong>, avec l'ajout de plusieurs entrées et niveaux de lecture. En effet, son arborescence reflète désormais la matrice du <strong>référentiel de la planification écologique.</strong> Véritable <strong>outil de pilotage destiné à la sphère publique</strong>, ce référentiel propose une vision systémique de nos objectifs de transition puisqu'il compile et met en cohérence <strong>des indicateurs</strong> pour l'ensemble des stratégies et plans dont s'est dotée la France pour les atteindre (Stratégie Nationale pour la Biodiversité, Stratégie nationale pour l'alimentation, la nutrition et le climat, Stratégie Nationale Bas-Carbone...). Et ce, en croisant :
+      </p>
+      <ul>
+        <li>les <strong>engagements de la France</strong> sur chacun des six objectifs environnementaux de la taxonomie européenne, soit l'état de l'environnement à date (ici figurés comme « indicateurs d'impact »)</li>
+        <li>les <strong>chantiers</strong> identifiés pour les respecter, dans chacun des secteurs</li>
+        <li>et <strong>les leviers</strong> opérationnels qui en découlent.</li>
+      </ul>
+      <p>
+        Ainsi, pour chaque engagement de la France pris à l'échelle européenne, plusieurs chantiers sectoriels sont mis en œuvre avec des leviers concrets pour les matérialiser. Référentiel comme baromètre partagent ainsi un <strong>double objectif de pédagogie et d'évaluation</strong>, appuyé par des principes et indicateurs tangibles, pour une appropriation de la transition écologique par tous, sans obérer son caractère parfois complexe et multifactoriel.
+      </p>
+    </div>
 
     <p class="fr-mt-4w">
       <a href="https://www.planification-ecologique.gouv.fr" class="fr-link" target="_blank" rel="noopener">
